489eb92390f294b2481da814faec16e885724f47
kenedy4 <kennedymusyoki8@gmail.com>
2024-09-12 10:46:54 +0300

    Updated the department relationship

--- lib/employee.py
+++ lib/employee.py
@@ -1,4 +1,3 @@
-# lib/employee.py
 from __init__ import CURSOR, CONN
 from department import Department
 
@@ -16,8 +15,8 @@ class Employee:
 
     def __repr__(self):
         return (
-            f"<Employee {self.id}: {self.name}, {self.job_title}>" +
-            f"department ID: {self.department_id}"
+            f"<Employee {self.id}: {self.name}, {self.job_title}, " +
+            f"Department ID: {self.department_id}>"
         )
 
     @classmethod
@@ -44,12 +43,12 @@ class Employee:
         CONN.commit()
 
     def save(self):
-        """ Insert a new row with the name and job title values of the current Employee object.
+        """ Insert a new row with the name, job title, and department id values of the current Employee object.
         Update object id attribute using the primary key value of new row.
         Save the object in local dictionary using table row's PK as dictionary key"""
         sql = """
                 INSERT INTO employees (name, job_title, department_id)
-                VALUES (?, ? ?)
+                VALUES (?, ?, ?)
         """
 
         CURSOR.execute(sql, (self.name, self.job_title, self.department_id))
@@ -62,10 +61,11 @@ class Employee:
         """Update the table row corresponding to the current Employee instance."""
         sql = """
             UPDATE employees
-            SET name = ?, job_title = ?, dpartment_id = ?
+            SET name = ?, job_title = ?, department_id = ?
             WHERE id = ?
         """
-        CURSOR.execute(sql, (self.name, self.job_title, self.department_id, self.id))
+        CURSOR.execute(sql, (self.name, self.job_title,
+                             self.department_id, self.id))
         CONN.commit()
 
     def delete(self):
@@ -103,9 +103,10 @@ class Employee:
             # ensure attributes match row values in case local instance was modified
             employee.name = row[1]
             employee.job_title = row[2]
+            employee.department_id = row[3]
         else:
             # not in dictionary, create new instance and add to dictionary
-            employee = cls(row[1], row[2])
+            employee = cls(row[1], row[2], row[3])
             employee.id = row[0]
             cls.all[employee.id] = employee
         return employee
